f9b398180435e950d8d5c3ae95efffc734e8af75
kartika <karkartika6@gmail.com>
2022-12-15 23:27:00 +0700

    add assignment

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Kartika" // TODO: replace this
+const studentId = "FE2844542" // TODO: replace this
+
+export { url, studentName, studentId }
